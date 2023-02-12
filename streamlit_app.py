ab70345259eed8dd73a2ffb8f3c0dad4193e8d5e
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-02-12 01:02:20 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+from streamlit_chat import message as st_message
+from transformers import BlenderbotTokenizer
+from transformers import BlenderbotForConditionalGeneration
+
+@st.experimental_singleton
+def get_models():
+    # it may be necessary for other frameworks to cache the model
+    # seems pytorch keeps an internal state of the conversation
+    model_name = "facebook/blenderbot-400M-distill"
+    tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
+    model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
+    return tokenizer, model
+
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+st.title("Hello Chatbot")
+
+
+def generate_answer():
+    tokenizer, model = get_models()
+    user_message = st.session_state.input_text
+    inputs = tokenizer(st.session_state.input_text, return_tensors="pt")
+    result = model.generate(**inputs)
+    message_bot = tokenizer.decode(
+        result[0], skip_special_tokens=True
+    )  # .replace("<s>", "").replace("</s>", "")
+
+    st.session_state.history.append({"message": user_message, "is_user": True})
+    st.session_state.history.append({"message": message_bot, "is_user": False})
+
+
+st.text_input("Talk to the bot", key="input_text", on_change=generate_answer)
+
+for chat in st.session_state.history:
+    st_message(**chat)  # unpacking
+
+"""def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return messageCreating the chatbot interface
+
+    st.title("chatBot : Streamlit + openAI")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    # store the output 
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')"""
