3563841e872f934cccba545aca6d962525455658
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-02-12 01:10:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ for chat in st.session_state.history:
     
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')"""
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
