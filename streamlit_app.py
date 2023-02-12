e095ad27ace94f0493c3091d3493c8b1e20ccf41
Smarcam <121159130+Smarcam@users.noreply.github.com>
2023-02-12 12:05:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,38 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
 def chatbot():
     st.title("Chatbot")
-    user_input = st.text_input("Enter your message:")
-    if user_input == "hi":
+    user_input = st.text_input("Enter your message:").lower()
+    if "hi" in user_input:
         st.write("Hello! How can I help you today?")
-    elif user_input == "what is your name?":
+    elif "name" in user_input:
         st.write("I am a chatbot created with Streamlit.")
-    elif user_input == "what do you do?":
+    elif "do" in user_input:
         st.write("I am here to assist you with any questions you may have.")
+    elif "information" in user_input:
+        query = user_input.replace("information", "").strip()
+        results = search_google(query)
+        st.write("Here's what I found on Google:")
+        for result in results:
+            st.write("- [{}]({})".format(result["title"], result["link"]))
     else:
         st.write("I'm sorry, I don't understand what you mean.")
+
+def search_google(query):
+    results = []
+    search_url = "https://www.google.com/search?q={}".format(query)
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    for g in soup.find_all("div", class_="r"):
+        anchors = g.find_all("a")
+        if anchors:
+            link = anchors[0]["href"]
+            title = g.find("h3").text
+            item = {"title": title, "link": link}
+            results.append(item)
+    return results
 
 if __name__ == '__main__':
     chatbot()
